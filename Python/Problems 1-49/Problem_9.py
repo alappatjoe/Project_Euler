039f80520a179426d2aec8f55446228cbc34c5eb
alappatjoe <alappatjoe@users.noreply.github.com>
2019-07-09 22:46:29 -0500

    Delete Problem_9.py

--- Python/Problems 1-49/Problem_9.py
+++
@@ -1,9 +0,0 @@
-# Project Euler Problem 9
-
-import math
-
-for a in range(501):
-    for b in range(501):
-        c = math.sqrt((a*a) + (b*b))
-        if c%1 == 0 and (a+b+c) == 1000:
-            print(a*b*c)
